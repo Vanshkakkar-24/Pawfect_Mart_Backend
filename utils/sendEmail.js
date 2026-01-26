44348e660bc5a9cbfebf0bd19bab5e8382fe24c0
Vansh Kakkar <kakkarvansh25@gmail.com>
2026-01-26 19:32:45 +0530

    Mail functionality using nodemailer

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,20 +1,35 @@
-import { Resend } from "resend";
 import dotenv from "dotenv";
 dotenv.config();
+import nodemailer from "nodemailer";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
 
 export const sendWelcomeEmail = async (to, name) => {
-    await resend.emails.send({
-        from: "PawfectMart <onboarding@resend.dev>",
-        to,
-        subject: "Welcome to PawfectMart 🐶🐱",
-        html: `
-      <h2>Hi ${name} 👋</h2>
-      <p>Welcome to <strong>PawfectMart</strong>!</p>
-      <p>We're excited to have you onboard 🐾</p>
-      <br/>
-      <p>- Team PawfectMart</p>
+  const info = await transporter.sendMail({
+    from: `"PawfectMart 🐾" <${process.env.EMAIL_USER}>`,
+    to,
+    subject: "Welcome to PawfectMart 🐶🐱",
+    html: `
+      <div style="font-family: Arial, sans-serif; line-height: 1.6">
+        <h2>Welcome to PawfectMart, ${name}! 🐾</h2>
+        <p>We're thrilled to have you with us.</p>
+        <p>
+          Explore premium pet products, exclusive offers,
+          and hassle-free shopping.
+        </p>
+        <p>
+          If you need help, just reply to this email or chat with us on WhatsApp 💚
+        </p>
+        <strong>- Team PawfectMart</strong>
+      </div>
     `,
-    });
+  });
 };
